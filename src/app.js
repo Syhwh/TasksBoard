bc707a785de533b8fb627266c83b247231b8e50e
Shymmy W. Garcia <shymmywgarcia@gmail.com>
2019-10-09 13:04:41 -0500

    Add handlebars templates

--- src/app.js
+++ src/app.js
@@ -10,20 +10,17 @@ const views = require('./views.js')
 
 views.homeView();
 
-$('#main').on('click','#createAccount',views.signUpView);
-$('#main').on('click','#backToLogin',views.homeView);
+$('#main').on('click', '#createAccount', views.signUpView);
+$('#main').on('click', '#backToLogin', views.homeView);
+$('#main').on('click', '#loginUser', auth.loginUser);
+$('#main').on('click', '#userSignUp', auth.signUpUser);
+$('#main').on('click', '#signOutUser', auth.signOutUser);
 
-$('#main').on('click','#loginUser', auth.loginUser);
-$('#main').on('click','#userSignUp',auth.signUpUser);
-$('#main').on('click','#signOutUser',auth.signOutUser);
-
-$('#main').on('keypress','#taskName',(e)=>{
-   
+$('#main').on('keypress', '#taskName', (e) => {
     const task = $(e.currentTarget).val()
-    const userID=auth.currentUser()
-    console.log(userID)
-    if (e.keyCode == 13){        
-        database.setTask(userID,task);
+    const userID = auth.currentUser()
+    if (e.keyCode == 13) {
+        database.setTask(userID, task);
         $(e.currentTarget).val('')
         database.getTasks(userID);
     }
